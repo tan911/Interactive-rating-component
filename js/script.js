0cad1b0865baec6570ce8a7a395df1512d46bfb1
Jovan Lanutan <jovanlanutan2001@Gmail.com>
2022-12-03 12:14:42 +0800

    re-create for solution

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const rateCard = document.querySelector('.card__inner--rate');
+const ratedCard = document.querySelector('.card__inner--message');
+const rateBtn = document.querySelectorAll('.card__rate');
+const submitBtn = document.querySelector('.btn');
+
+const ratedModal = function () {
+  ratedCard.classList.remove('hidden');
+  rateCard.classList.add('hidden');
+};
+
+for (let i = 0; i < rateBtn.length; i++) {
+  rateBtn[i].addEventListener('click', () => {
+    document.querySelector('span').textContent = rateBtn[i].textContent;
+  });
+}
+
+submitBtn.addEventListener('click', ratedModal);
